6b6abfb9eb054f1b51fcb169bc12807c0fac0079
Jonas Rhbary <rhbaryj@bzz.ch>
2022-06-08 14:10:47 +0200

    View Package

--- src/view/View.java
+++ src/view/View.java
@@ -9,16 +9,31 @@ import java.awt.event.*;
  */
 class View implements ActionListener{
     JFrame f;
+    JFrame uebersichtFrame;
+    JFrame zuordnungFrame;
+    JFrame personenFrame;
+    JFrame stammdatenFrame;
+    JFrame logbuchFrame;
+
+    JTextField test;
+
     JMenuBar mb;
     JMenu uebersicht,zuordnung,personen,stammdaten,logbuch;
     View(){
         f=new JFrame();
         mb=new JMenuBar();
         uebersicht=new JMenu("Übersicht");
-        zuordnung=new JMenu("Zurodnung");
+        zuordnung=new JMenu("Zuordnung");
         personen=new JMenu("Personen");
         stammdaten=new JMenu("Stammdaten");
         logbuch=new JMenu("Logbuch");
+
+        uebersicht.addActionListener(this);
+        zuordnung.addActionListener(this);
+        personen.addActionListener(this);
+        stammdaten.addActionListener(this);
+        logbuch.addActionListener(this);
+
 
         mb.add(uebersicht);mb.add(zuordnung);mb.add(personen);mb.add(stammdaten);mb.add(logbuch);
         f.add(mb);
@@ -26,8 +41,27 @@ class View implements ActionListener{
         f.setLayout(null);
         f.setSize(400,400);
         f.setVisible(true);
+
     }
     public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == uebersicht)
+            uebersichtFrame = new JFrame();
+                uebersichtFrame.add(mb);
+                uebersichtFrame.setJMenuBar(mb);
+                uebersichtFrame.setLayout(null);
+                uebersichtFrame.setSize(400,400);
+                uebersichtFrame.setVisible(true);
+                uebersichtFrame.add(test);
+
+        if(e.getSource() == zuordnung)
+            zuordnungFrame = new JFrame();
+        if(e.getSource() == personen)
+            personenFrame = new JFrame();
+        if(e.getSource() == stammdaten)
+            stammdatenFrame = new JFrame();
+        if(e.getSource() == logbuch)
+            logbuchFrame = new JFrame();
+
     }
     public static void main(String[] args) {
         new View();
