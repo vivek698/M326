4bc238c9c2d452cac97489370250d50e7e543403
Jonas Rhbary <76094114+jonaskrt@users.noreply.github.com>
2022-06-08 13:41:01 +0200

    Add files via upload

---
+++ src/log/View.java
@@ -0,0 +1,35 @@
+package log;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+/**
+ * M326 View Version 0.1
+ * Jonas Rhbary
+ */
+class View implements ActionListener{
+    JFrame f;
+    JMenuBar mb;
+    JMenu uebersicht,zuordnung,personen,stammdaten,logbuch;
+    View(){
+        f=new JFrame();
+        mb=new JMenuBar();
+        uebersicht=new JMenu("Übersicht");
+        zuordnung=new JMenu("Zurodnung");
+        personen=new JMenu("Personen");
+        stammdaten=new JMenu("Stammdaten");
+        logbuch=new JMenu("Logbuch");
+
+        mb.add(uebersicht);mb.add(zuordnung);mb.add(personen);mb.add(stammdaten);mb.add(logbuch);
+        f.add(mb);
+        f.setJMenuBar(mb);
+        f.setLayout(null);
+        f.setSize(400,400);
+        f.setVisible(true);
+    }
+    public void actionPerformed(ActionEvent e) {
+    }
+    public static void main(String[] args) {
+        new View();
+    }
+}
