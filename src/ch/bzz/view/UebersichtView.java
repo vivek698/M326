de792dd1069ddfc37fe276b0324d1016c68997b2
Niki <vogeln@bzz.ch>
2022-06-21 18:17:43 +0200

    added UebersichtView to mainView

--- src/ch/bzz/view/UebersichtView.java
+++ src/ch/bzz/view/UebersichtView.java
@@ -2,6 +2,15 @@ package ch.bzz.view;
 
 import javax.swing.*;
 
-public class UebersichtView extends JPanel {
-    public UebersichtView(){}
+public class UebersichtView extends JPanel{
+    private PersonComponent personComponent;
+    private JPanel panel = new JPanel();
+    public UebersichtView(){
+        personComponent = new PersonComponent(panel);
+
+
+
+        add(personComponent);
+        setVisible(true);
+    }
 }
